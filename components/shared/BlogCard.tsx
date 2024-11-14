b10ebffaed7dbbbbeb69b8ef386e5356d9f3d55a
molaraiche <laraichemohamed@gmail.com>
2024-11-14 01:43:25 +0100

    updates

--- components/shared/BlogCard.tsx
+++ components/shared/BlogCard.tsx
@@ -27,15 +27,17 @@ const BlogCard: React.FC<blogPostType> = ({
         },
       }}
       className='flex flex-col items-center justify-center gap-4 lg:w-[396px] md:w-[396px] sm:w-[90%] xsm:w-[90%] p-4 rounded-xl border border-secondary-100 shadow-sm'>
-      <div className=''>
+      <div className='w-[360px] h-[240px] overflow-hidden'>
         <Image
           src={thumbnail}
           alt={title}
           width={360}
           height={240}
-          className='w-[360px] h-[240px] object-cover'
+          className='object-cover'
+          layout='fixed'
         />
       </div>
+
       <div className='flex flex-col gap-5'>
         <div className=''>
           <p className='text-[#4B6BFB] font-medium bg-tagBg w-fit py-1 px-2.5 rounded-md'>
